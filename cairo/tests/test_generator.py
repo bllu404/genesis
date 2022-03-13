389b2d6a564ce44a32479dade85c5a77961efb8e
bllu404 <96asdffdsa69@protonmail.com>
2022-03-12 21:03:06 -0500

    slice visualization

--- cairo/tests/test_generator.py
+++ cairo/tests/test_generator.py
@@ -2,6 +2,7 @@ import os
 
 import pytest
 from starkware.starknet.testing.starknet import Starknet
+from matplotlib import pyplot as plt
 
 # The path to the contract source code.
 CONTRACT_FILE = os.path.join("contracts", "generator.cairo")
@@ -19,14 +20,20 @@ async def test_terrain_generator():
     contract = await starknet.deploy(
         source=CONTRACT_FILE,
     )
-    
+    plt.axes()
+    colors = ["#03b1fc", "#696969", "#703307", "#347812", "#f5c011"]
     # Testing generate_block method
     for i in range(20):
-        block = await contract.generate_block(10,10,85+i).invoke()
-        print()
-        print(block.result.block_type)
-        print(block.call_info.cairo_usage.n_steps)
+        for j in range(20):
+            block = await contract.generate_block(i,10,85+j).invoke()
+            #print()
+            #print(block.result.block_type)
+            #print(block.call_info.cairo_usage.n_steps)
+            square = plt.Rectangle((5*i,5*j), 5, 5, fc=colors[block.result.block_type - 1])
+            plt.gca().add_patch(square)
     
+    plt.axis('scaled')
+    plt.savefig('terrain.png')
     '''
     block = await contract.generate_block(10,10,100).invoke()
     print(block.result.block_type)
