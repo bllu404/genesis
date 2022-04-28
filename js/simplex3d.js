22590759019ff15c5e27316b01c0782acfb2c8fa
bllu404 <96asdffdsa69@protonmail.com>
2022-04-28 15:18:18 +0300

    generator algo, not working yet

--- js/simplex3d.js
+++ js/simplex3d.js
@@ -5,12 +5,17 @@
 //
 //
 // ***************************************************************************** //
+
+// Simplex noise constants
 const F = 768614336404564650n
 const G = 384307168202282325n
 const R_SQUARED = 1383505805528216371n
 const ONE = BigInt(2**61)
+
+// Perlin noise constants
 const HALF_SQRT = 1630477227105714176n
 
+//Gradient generation
 const gradients = [
     [ONE,ONE,0n],
     [-ONE,ONE,0n],
@@ -52,6 +57,54 @@ const p = [
     138,236,205,93,222,114,67,29,24,72,243,141,128,195,78,66,215,61,156,180
 ]
 
+// Procedural generation constants
+const UNINITIALIZED = 0;
+const AIR = 1;
+const STONE = 2;
+const DIRT = 3;
+const GRASS = 4;
+const ORE = 5; 
+const WOOD = 6;
+const LEAF = 7;
+
+// Defining the weight of each octave (64.61 format)
+const HEIGHTMAP_OCTAVE1_W = 1152921504606846976n  // 0.5
+const HEIGHTMAP_OCTAVE2_W = 691752902764108185n  // 0.3
+const HEIGHTMAP_OCTAVE3_W = 461168601842738790n  // 0.2
+
+// Defining the scale of each octave (side-lengths of the grid squares)
+const HEIGHTMAP_OCTAVE1_S = 300n
+const HEIGHTMAP_OCTAVE2_S = 100n
+const HEIGHTMAP_OCTAVE3_S = 50n
+
+// At an amplitude of 70, the difference in height between the lowest point on the surface of the terrain and the tallest is approximately 100.
+// This is because the perlin noise function outputs a maximum value of ~0.7071 and a minimum value of about -0.7071
+const SURFACE_AMPLITUDE = 70n
+const SURFACE_BASELINE = 100n * ONE  // At a baseline of 100 and amplitude of 50, the tallest block generated can have a height of 150.
+
+// How many blocks below the surface the soil goes before stone is reached
+const TOPSOIL_BASELINE = 8n * ONE
+
+// Maximum displacement (in either direction) of the baseline.
+const TOPSOIL_AMPLITUDE = 5n
+
+// Scale factor to be used in noise function for soil
+const TOPSOIL_SCALE = 50n
+
+// Defining the weight of each octave (64.61 format)
+const CAVE_OCTAVE1_W = 1152921504606846976n // 0.5
+const CAVE_OCTAVE2_W = 691752902764108185n // 0.3
+const CAVE_OCTAVE3_W = 461168601842738790n  // 0.2
+
+// Defining the scale of each octave (side-lengths of the grid squares)
+const CAVE_OCTAVE1_S = 20n
+const CAVE_OCTAVE2_S = 10n
+const CAVE_OCTAVE3_S = 5n
+
+// A Fractal noise value above this value means no block is there, otherwise a block is there. 
+const CAVE_THRESHOLD = 161409010644958576n // 0.07
+
+
 // ********************************* MAIN ********************************* // 
 //
 //
@@ -61,12 +114,17 @@ const p = [
 // *********************************************************************** //
 console.log("\nAlong x-axis");
 for (let i = 0n; i < 20n; i++) {
-    console.log(noise3DCustom(100n - i,100n,100n, 100n, 69n));
+    console.log(Number(noise3DCustom(100n - i,100n,100n, 100n, 69n))/Number(ONE));
 }
 
 console.log("\nAlong x-axis - perlin");
 for (let i = 0n; i < 20n; i++) {
-    console.log(noise2DCustom(150n - i,150n, 100n, 69n));
+    console.log(Number(noise2DCustom(150n - i,150n, 100n, 69n))/Number(ONE));
+}
+
+console.log("generator");
+for (let i = 0n; i < 20n; i++) {
+    console.log(generateBlock(50n, 50n, 80n + i));
 }
 /*
 console.log("\nAlong y-axis");
@@ -78,6 +136,62 @@ console.log("\nAlong z-axis");
 for (let i = 0; i < 20; i++) {
     console.log(noise3DCustom(100,100 + i,100 + i, 100, 69)/ONE);
 }*/
+
+// ********************************* PROCEDURAL TERRAIN GENERATION ********************************* // 
+//
+//
+//
+//
+//
+// *********************************************************************** //
+
+function generateBlock(x,y,z) {
+    let noise1 = noise2DCustom(x, y, HEIGHTMAP_OCTAVE1_S, 69)
+    let noise2 = noise2DCustom(x, y, HEIGHTMAP_OCTAVE2_S, 420)
+    let noise3 = noise2DCustom(x, y, HEIGHTMAP_OCTAVE3_S, 42069)
+
+    let octave1 = mul(noise1, HEIGHTMAP_OCTAVE1_W)
+    let octave2 = mul(noise2, HEIGHTMAP_OCTAVE2_W)
+    let octave3 = mul(noise3, HEIGHTMAP_OCTAVE3_W)
+
+    let surfaceHeight = from64x61(SURFACE_AMPLITUDE * (octave1 + octave2 + octave3) + SURFACE_BASELINE)
+
+    if (z == surfaceHeight) {
+        return GRASS;
+    } else if (z < surfaceHeight) {
+        let soilDisplacementNoise = noise2DCustom(x,y, TOPSOIL_SCALE);
+        let soilDepth = from64x61(TOPSOIL_BASELINE + TOPSOIL_AMPLITUDE * soilDisplacementNoise);
+
+        if (surfaceHeight - soilDepth <= z) {
+            return DIRT;
+
+        } else {
+            let noise1 = noise3DCustom(x,y,z, CAVE_OCTAVE1_S, 69);
+            let noise2 = noise3DCustom(x,y,z, CAVE_OCTAVE2_S, 420);
+            let noise3 = noise3DCustom(x,y,z, CAVE_OCTAVE2_S, 42069);
+
+            let octave1 = mul(noise1, CAVE_OCTAVE1_W);
+            let octave2 = mul(noise2, CAVE_OCTAVE2_W);
+            let octave3 = mul(noise3, CAVE_OCTAVE3_W);
+
+            let sum = octave1 + octave2 + octave3;
+            if (CAVE_THRESHOLD <= sum) {
+                return AIR;
+            } else {
+                let isOre = perlinRandNum(x,y,z) % 8;
+                if (isOre) {
+                    return STONE;
+                } else {
+                    return ORE;
+                }
+            }
+        }
+    } else {
+        return AIR;
+    }
+}
+
+
 
 // ********************************* PERLIN NOISE ********************************* // 
 //
